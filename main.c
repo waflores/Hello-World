cd26e4edd23c45a4d7de8ba36a11a87e8b164556
waflores <waflores@ncsu.edu>
2012-01-26 23:17:51 -0500

    got a function to pull

--- main.c
+++ main.c
@@ -8,13 +8,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void printSomething(void);
 /*
  * 
  */
 int main(int argc, char** argv) {
     /* Yeah... it's very cliche'd; but it will get the job done */
     printf("Hello world!\r\n");
+    printSomething();
     fflush(stdout);
     return (EXIT_SUCCESS);
 }
 
+void printSomething(void) {
+    /* yeahhhhhhh */
+    printf("Something\r\n");
+    fflush(stdout);
+    return;
+}
