3bc965359ce0ff3f3295743c56e2ecb545f822af
Will Flores <waflores@ncsu.edu>
2012-01-26 22:37:17 -0500

    using 'skip adding new or moified ... directly to head'

---
+++ main.c
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.c
+ * Author: Will
+ *
+ * Created on January 25, 2012, 11:42 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    /* Yeah... it's very cliche'd; but it will get the job done */
+    printf("Hello world!\r\n");
+    return (EXIT_SUCCESS);
+}
+
